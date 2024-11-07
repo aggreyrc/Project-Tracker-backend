29b3beb1720190b9fe52d1b49cc533fc2f19f851
Monalisa Onyango <+>
2024-11-08 01:09:54 +0300

    minor adjustments on app.py

--- server/app.py
+++ server/app.py
@@ -3,10 +3,11 @@
 from flask import request, make_response, session, Flask
 from flask_migrate import Migrate
 from flask_restful import Resource,Api
-from models import User, Project, Cohort, ProjectMember
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+
+from models import User, Project, Cohort, ProjectMember,db
 
 import os
 
@@ -18,7 +19,6 @@ app.config['SECRET_KEY'] = b'|\xab_\xac<\xcb\xe2\xc8~\x110\x82\xeb\xfa\xc8~'
 app.json.compact = False
 
 
-db = SQLAlchemy()
 api = Api(app)
 CORS(app)
 
