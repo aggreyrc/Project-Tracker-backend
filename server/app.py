c09c112d892308f5c2c98dec1f6f7ad0effd2659
aggreyrc <aggreyronoh@gmail.com>
2024-11-08 16:26:07 +0300

    edited port

--- server/app.py
+++ server/app.py
@@ -53,4 +53,5 @@ class Home(Resource):
 api.add_resource(Home, '/')
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+     port = int(os.environ.get("PORT", 5555))
+     app.run(host="0.0.0.0", port=port, debug=True)
