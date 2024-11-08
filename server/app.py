d97f60b4500650f1f251ec0d92fec87e97475da4
aggreyrc <aggreyronoh@gmail.com>
2024-11-08 16:11:28 +0300

    file config

--- server/app.py
+++ server/app.py
@@ -4,9 +4,9 @@ import os
 from flask import request, make_response, session, Flask, jsonify
 from flask_migrate import Migrate
 from flask_restful import Resource,Api
-from flask_bcrypt import Bcrypt
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from flask_cors import CORS
+# from flask_sqlalchemy import SQLAlchemy
 
 from models import User, db
 
